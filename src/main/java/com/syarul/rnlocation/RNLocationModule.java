5228b480b22ad267437c0e0d47136f6a9c36c861
andrew.stupchuk <andrii.stupchuk@eleks.com>
2018-03-09 15:01:47 +0200

    -remove getLastKnownLocation from constructor

--- src/main/java/com/syarul/rnlocation/RNLocationModule.java
+++ src/main/java/com/syarul/rnlocation/RNLocationModule.java
@@ -43,7 +43,6 @@ public class RNLocationModule extends ReactContextBaseJavaModule{
     mReactContext = reactContext;
 
     locationManager = (LocationManager) mReactContext.getSystemService(Context.LOCATION_SERVICE);
-    mLastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
   }
 
 
